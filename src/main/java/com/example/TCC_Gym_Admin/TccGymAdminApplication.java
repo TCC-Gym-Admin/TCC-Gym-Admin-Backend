054357be560a4c4766ff7820e022fde42f6b1532
Pedro Henrique <ph8584774@gmail.com>
2026-02-21 12:28:19 -0300

    Commit inicial

---
+++ src/main/java/com/example/TCC_Gym_Admin/TccGymAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.TCC_Gym_Admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccGymAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccGymAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TCC_Gym_Admin/TccGymAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TCC_Gym_Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccGymAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
